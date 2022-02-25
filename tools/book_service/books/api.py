b28363f78ceffe880a00caf90152ec719bc65eb5
Scott Hendrickson <scott@drskippy.net>
2022-02-24 21:26:28 -0700

    Added endpoints, normalized behavior

--- tools/book_service/books/api.py
+++ tools/book_service/books/api.py
@@ -1,56 +1,21 @@
 __version__ = '0.1.0'
 
-import datetime
-import json
-import logging
-from decimal import Decimal
-
 import pymysql
-# import requests
-# import random
-from flask import Flask, Response
+from flask import Flask, Response, request
 
-config_filename = "./config/configuration.json"
-with open(config_filename, "r") as config_file:
-    logging.debug(f"opening configuration file {config_filename}")
-    c = json.load(config_file)
-    logging.debug("{}".format(c))
-    try:
-        UN = c["username"].strip()
-        PWD = c["password"].strip()
-        DB = c["database"].strip()
-        DBHOST = c["host"].strip()
-    except KeyError as e:
-        logging.error(e)
-        sys.exit()
+from books_util import *
 
-db = pymysql.connect(host=DBHOST, port=3306, user=UN, passwd=PWD, db=DB)
-
+conf = get_configuration()
+db = pymysql.connect(**conf)
 # server configuration
 app = Flask(__name__)
 
 
-def serialize_rows(cursor):
-    res = []
-    for row in cursor:
-        _row = []
-        for d in row:
-            if isinstance(d, Decimal):
-                _row.append(float(d))
-            elif isinstance(d, datetime.datetime):
-                _row.append(d.strftime("%Y-%m-%d"))
-            else:
-                _row.append(d)
-        app.logger.debug(f"row={_row}")
-        res.append(_row)
-    return json.dumps(res)
-
-
-#################################################
-
-@app.route('/')
-def main():
-    rdata = json.dumps({"key": "Hello World"})
+@app.route('/configuration')
+def configuration():
+    rdata = json.dumps({
+        "configuration": conf,
+        "date": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M")})
     response_headers = [
         ('Content-type', 'application/json'),
         ('Content-Length', str(len(rdata)))
@@ -58,45 +23,74 @@ def main():
     return Response(response=rdata, status=200, headers=response_headers)
 
 
-@app.route('/tags')
-def show_tags():
-    rdata = []
-    c = db.cursor()
-    try:
-        c.execute("SELECT Tag, COUNT(Tag) as count FROM tags GROUP BY Tag ORDER BY count DESC")
-    except pymysql.Error as e:
-        app.logger.error(e)
-    else:
-        s = c.fetchall()
-        rdata = serialize_rows(s)
-    response_headers = [
-        ('Content-type', 'application/json'),
-        ('Content-Length', str(len(rdata)))
-    ]
-    return Response(response=rdata, status=200, headers=response_headers)
-
-
-@app.route('/books_year/<target_year>')
-@app.route('/books_year')
-def books_year(target_year=None):
-    rdata = []
-    search_str = """ SELECT YEAR(LastRead), SUM(Pages) as Pages, COUNT(Pages) as Books
-                FROM `book collection`
-                WHERE LastRead is not NULL and LastRead <> "0000-00-00 00:00:00" and year(LastRead) <> "1966" """
-    if target_year is not None:
-        search_str += f" and YEAR(LastRead) = {target_year}"
-    search_str += """
-                GROUP BY YEAR(LastRead)
-                ORDER BY LastRead ASC;"""
+@app.route('/tag_counts')
+@app.route('/tag_counts/<tag>')
+def tag_counts(tag=None):
+    search_str = "SELECT Tag, COUNT(Tag) as count FROM tags"
+    if tag is not None:
+        search_str += f" WHERE Tag LIKE \"{tag}%\""
+    search_str += " GROUP BY Tag ORDER BY count DESC"
+    header = ["tag", "count" ]
     app.logger.debug(search_str)
     c = db.cursor()
     try:
         c.execute(search_str)
     except pymysql.Error as e:
         app.logger.error(e)
+        rdata = json.dumps({"error": str(e)})
     else:
         s = c.fetchall()
-        rdata = serialize_rows(s)
+        rdata = serialize_rows(s, header)
+    response_headers = [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(rdata)))
+    ]
+    return Response(response=rdata, status=200, headers=response_headers)
+
+
+@app.route('/tags/<id>')
+def tags(id=None):
+    search_str = f"SELECT Tag FROM tags WHERE BookID = {id} ORDER BY Tag"
+    app.logger.debug(search_str)
+    c = db.cursor()
+    try:
+        c.execute(search_str)
+    except pymysql.Error as e:
+        app.logger.error(e)
+        rdata = json.dumps({"error": str(e)})
+        rdata = {"error": str(e)}
+    else:
+        s = c.fetchall()
+        tag_list = [x[0] for x in s]
+        s = list(s)
+        rdata = json.dumps({"BookID": f"{id}", "tag_list": f"{tag_list}"})
+    response_headers = [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(rdata)))
+    ]
+    return Response(response=rdata, status=200, headers=response_headers)
+
+@app.route('/books_read_by_year/<target_year>')
+@app.route('/books_read_by_year')
+def books_read_by_year(target_year=None):
+    search_str = ("SELECT YEAR(LastRead) as Year, SUM(Pages) as Pages, COUNT(Pages) as Books\n"
+                  "FROM `book collection`\n"
+                  "WHERE LastRead is not NULL and LastRead <> \"0000-00-00 00:00:00\" and year(LastRead) <> \"1966\" ")
+    if target_year is not None:
+        search_str += f" AND YEAR(LastRead) = {target_year}\n"
+    search_str += ("GROUP BY YEAR(LastRead)\n"
+                   "ORDER BY LastRead ASC;")
+    header = ["year", "pages read", "books read"]
+    app.logger.debug(search_str)
+    c = db.cursor()
+    try:
+        c.execute(search_str)
+    except pymysql.Error as e:
+        app.logger.error(e)
+        rdata = json.dumps({"error": str(e)})
+    else:
+        s = c.fetchall()
+        rdata = serialize_rows(s, header)
     response_headers = [
         ('Content-type', 'application/json'),
         ('Content-Length', str(len(rdata)))
@@ -107,23 +101,90 @@ def books_year(target_year=None):
 @app.route('/books_read/<target_year>')
 @app.route('/books_read')
 def books_read(target_year=None):
-    rdata = []
-    search_str = """ SELECT *
-                FROM `book collection`
-                WHERE LastRead is not NULL and LastRead <> "0000-00-00 00:00:00" """
+    search_str = ("SELECT *\n"
+                  "FROM `book collection`\n"
+                  "WHERE LastRead is not NULL and LastRead <> \"0000-00-00 00:00:00\" ")
     if target_year is not None:
         search_str += f" and YEAR(LastRead) = {target_year}"
-    search_str += """
-                ORDER BY LastRead ASC;"""
+    search_str += " ORDER BY LastRead ASC;"
+    header = table_header
     app.logger.debug(search_str)
     c = db.cursor()
     try:
         c.execute(search_str)
     except pymysql.Error as e:
         app.logger.error(e)
+        rdata = json.dumps({"error": str(e)})
     else:
         s = c.fetchall()
-        rdata = serialize_rows(s)
+        rdata = serialize_rows(s, header)
+    response_headers = [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(rdata)))
+    ]
+    return Response(response=rdata, status=200, headers=response_headers)
+
+
+@app.route('/books')
+def books():
+    # process any query parameters
+    args = request.args
+    where = []
+    for key in args:
+        if key == "BookCollectionID":
+            where.append(f"{key} = \"{args.get(key)}\"")
+        else:
+            where.append(f"{key} LIKE \"%{args.get(key)}%\"")
+    where_str = "AND".join(where)
+    # run the query
+    search_str = ("SELECT *\n"
+                  "FROM `book collection`")
+    if where_str != '':
+        search_str += "\nWHERE " + where_str
+    header = table_header
+    app.logger.debug(search_str)
+    c = db.cursor()
+    try:
+        c.execute(search_str)
+    except pymysql.Error as e:
+        app.logger.error(e)
+        rdata = json.dumps({"error": str(e)})
+    else:
+        s = c.fetchall()
+        rdata = serialize_rows(s, header)
+    response_headers = [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(rdata)))
+    ]
+    return Response(response=rdata, status=200, headers=response_headers)
+
+
+@app.route('/update_tag_value/<current>/<updated>')
+def update_tag_value(current, updated):
+    c = db.cursor()
+    try:
+        records = c.execute("UPDATE `tags` SET Tag = '{}' WHERE Tag = '{}'".format(updated.lower().strip(), current))
+        db.commit()
+        rdata = json.dumps({"tag_update": f"{current} >> {updated}", "updated_tags": records})
+    except pymysql.Error as e:
+        app.logger.error(e)
+        rdata = json.dumps({"error": str(e)})
+    response_headers = [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(rdata)))
+    ]
+    return Response(response=rdata, status=200, headers=response_headers)
+
+
+@app.route('/add_tag_by_id/<id>/<tag>')
+def add_tag(self, id, tag):
+    c = db.cursor()
+    try:
+        c.execute(f"insert into tags (BookID, Tag) values (%s, %s)", (id, tag))
+        rdata = json.dumps({"BookID": f"{id}", "Tag": f"{tag}"})
+    except pymysql.Error as e:
+        app.logger.error(e)
+        rdata = json.dumps({"error": str(e)})
     response_headers = [
         ('Content-type', 'application/json'),
         ('Content-Length', str(len(rdata)))
---
+++ tools/book_service/books/books_util.py
@@ -0,0 +1,50 @@
+import datetime
+import json
+from decimal import Decimal
+
+table_header = ["BookCollectionID", "Title", "Author", "CopyrightDate", "ISBNNumber", "PublisherName",
+                "CoverType", "Pages", "LastRead", "PrevLastRead", "Category", "Note", "Recycled",
+                "Location", "ISBNNumber13"]
+
+
+def get_configuration():
+    config_filename = "./config/configuration.json"
+    with open(config_filename, "r") as config_file:
+        c = json.load(config_file)
+        try:
+            res = {
+                "user": c["username"].strip(),
+                "passwd": c["password"].strip(),
+                "db": c["database"].strip(),
+                "host": c["host"].strip(),
+                "port": int(c["port"])
+            }
+        except KeyError as e:
+            print(e)
+            sys.exit()
+        return res
+
+
+def serialize_rows(cursor, header=None):
+    """
+    Provide a header here or output header will be None
+    :param cursor: iterable results from pymysql query
+    :param header: list of strings describing data to keep
+    :return: json payload
+    """
+    result = {"header": header, "data": []}
+    result_rows = []
+    for row in cursor:
+        _row = []
+        if len(header) != len(row):
+            print("mismatched header to data provided")
+        for d in row:
+            if isinstance(d, Decimal):
+                _row.append(float(d))
+            elif isinstance(d, datetime.datetime):
+                _row.append(d.strftime("%Y-%m-%d"))
+            else:
+                _row.append(d)
+        result_rows.append(_row)
+    result["data"] = result_rows
+    return json.dumps(result)
