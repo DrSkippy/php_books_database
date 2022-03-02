01de5314432d4dc57884fb3977f3a61f9fa59f28
DrSkippy <scott@aspenmesh.io>
2022-03-02 14:11:50 -0700

    working on repl tool

--- tools/bookdbtool/repl_tools.py
+++ tools/bookdbtool/repl_tools.py
@@ -5,21 +5,57 @@ import requests
 
 class REPL_Tool:
     ENDPOINT = "http://192.168.127.8/books"
+    PAGE_SIZE = 20
 
     def __init__(self):
         pass
 
-    def show_tags(self, bookid=None):
+    def help(self, ref):
+        """List available functions for accessing book collection database."""
+        for x in dir(ref):
+            if not x.startswith("_"):
+                print("-"*40)
+                print(x)
+                print("-"*40)
+                print(exec(f"ref.{x}.__doc__"))
+
+
+    def _reduce_string(self, row, idxs):
+        return [row[i] for i in idxs]
+
+
+    def _print_rows(self, data, header, indexes, format_string):
+        if len(data) == 0:
+            print("No data")
+        else:
+            hs = format_string.replace("d", "s").format(*self._reduce_string(header, indexes))
+            print(hs)
+            print("-"*len(hs))
+            for i, row in enumerate(data):
+                print(format_string.format(*self._reduce_string(row, indexes)))
+                if i % self.PAGE_SIZE == self.PAGE_SIZE - 1:
+                    a = input("<return> to cont, q to quit...")
+                    if a.lower().startswith("q"):
+                        break
+        return
+
+
+    def tag_counts(self, tag=None):
+        """ Takes 0 or 1 arguments. If an argument is provided, only tags matching this root string will appear. """
+        q = self.ENDPOINT + "/tag_counts"
+        if tag is not None:
+            q += f"/{tag}"
         try:
-            r = requests.get(self.ENDPOINT + "/tag_counts")
+            r = requests.get(q)
             res = r.json()
         except requests.RequestException as e:
             logging.error(e)
         else:
-            for j in res["data"]:
-                print("{:25s}: {:4d}".format(*j))
+            fmt = "{:25s}: {:8d}"
+            self._print_rows(res["data"], res["header"], [0, 1], fmt)
 
     def books(self, **query):
+        """ Takes 0 or many arguments. E.g. Title="two citites", Author="Dickens" """
         q = self.ENDPOINT + "/books?"
         first = True
         for k, v in query.items():
@@ -34,11 +70,11 @@ class REPL_Tool:
         except requests.RequestException as e:
             logging.error(e)
         else:
-            for j in res["data"]:
-                print("{:12d} | {:75s}\n             | {:25s}".format(*j[:3]))
+            fmt = "{:12d} | {:75s}\n             | {:25s}"
+            self._print_rows(res["data"], res["header"], [0, 1, 2], fmt)
 
 
 if __name__ == "__main__":
     rpl = REPL_Tool()
-    rpl.show_tags()
+    rpl.tag_counts()
     rpl.books(Title="science")
