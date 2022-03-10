da7430588645bae1975c31726af94b09ce45f24b
Scott Hendrickson <scott@drskippy.net>
2022-03-09 21:17:24 -0700

    Added yearly report function

--- tools/bookdbtool/repl_tools.py
+++ tools/bookdbtool/repl_tools.py
@@ -1,32 +1,37 @@
 import logging
 
 import requests
+from columnar import columnar
 
 
 class BC_Tool:
     ENDPOINT = "http://192.168.127.8/books"
     PAGE_SIZE = 20
+    MINIMAL_BOOK_INDEXES = [0, 1, 2, 6, 7, 8]
 
     def __init__(self):
         pass
 
-    def _column_selector(self, row, idxs):
-        return [row[i] for i in idxs]
+    def _row_column_selector(self, row, indexes):
+        return [row[i] for i in indexes]
 
-    def _print_rows(self, data, header, indexes, format_string):
-        if len(data) == 0:
-            print("No data")
+    def _column_selector(self, data, indexes):
+        return [self._row_column_selector(i, indexes) for i in data]
+
+    def _show_table(self, data, header, indexes):
+        table = columnar(self._column_selector(data, indexes), self._row_column_selector(header, indexes))
+        print(table)
+
+    def version(self):
+        """ Retrieve the back end version. """
+        q = self.ENDPOINT + "/configuration"
+        try:
+            r = requests.get(q)
+            res = r.json()
+        except requests.RequestException as e:
+            logging.error(e)
         else:
-            hs = format_string.replace("d", "s").format(*self._column_selector(header, indexes))
-            print(hs)
-            print("-" * len(hs))
-            for i, row in enumerate(data):
-                print(format_string.format(*self._column_selector(row, indexes)))
-                if i % self.PAGE_SIZE == self.PAGE_SIZE - 1:
-                    a = input("<return> to cont, q to quit...")
-                    if a.lower().startswith("q"):
-                        break
-        return
+            print("Version: {}".format(res["version"]))
 
     def tag_counts(self, tag=None):
         """ Takes 0 or 1 arguments. If an argument is provided, only tags matching this root string will appear. """
@@ -39,8 +44,7 @@ class BC_Tool:
         except requests.RequestException as e:
             logging.error(e)
         else:
-            fmt = "{:25s}: {:8d}"
-            self._print_rows(res["data"], res["header"], [0, 1], fmt)
+            self._show_table(res["data"], res["header"], [0, 1])
 
     def books(self, **query):
         """ Takes 0 or many arguments. E.g. Title="two citites", Author="Dickens" """
@@ -58,33 +62,62 @@ class BC_Tool:
         except requests.RequestException as e:
             logging.error(e)
         else:
-            fmt = "{:12d} | {:75s}\n             | {:28s} | {:8s} | {:12s}"
-            self._print_rows(res["data"], res["header"], [0, 1, 2, 6, 8], fmt)
+            self._show_table(res["data"], res["header"], self.MINIMAL_BOOK_INDEXES)
 
-    def book(self, id):
+    def book(self, book_collection_id):
         """ Takes 1 argument."""
-        q = self.ENDPOINT + f"/books?BookCollectionID={id}"
+        q = self.ENDPOINT + f"/books?BookCollectionID={book_collection_id}"
         try:
             r = requests.get(q)
             res = r.json()
         except requests.RequestException as e:
             logging.error(e)
         else:
-            q = self.ENDPOINT + f"/tags/{id}"
+            q = self.ENDPOINT + f"/tags/{book_collection_id}"
             try:
                 tr = requests.get(q)
                 tres = tr.json()
             except requests.RequestException as e:
                 logging.error(e)
             else:
-                fmt = "{:12d} | {:75s}\n             | {:28s} | {:8s} | {:12s}"
-                self._print_rows(res["data"], res["header"], [0, 1, 2, 6, 8], fmt)
-                if len(tres["tag_list"]) > 0:
-                    fmt = "{} |"*(len(tres["tag_list"])-1)
-                    fmt += " {}"
-                    print(fmt.format(*tres["tag_list"]))
+                _template = ["" for i in range(len(res["data"][0]))]
+                _template[0] = "    Tags:"
+                _data = []
+                for d in res["data"]:
+                    _data.append(d)
+                    if len(tres["tag_list"]) > 0:
+                        _template[1] = "\n".join(tres["tag_list"])
+                        _data.append(_template)
+                self._show_table(_data, res["header"], self.MINIMAL_BOOK_INDEXES)
+
+    def books_read_by_year(self, year=None):
+        """ Takes 1 argument."""
+        q = self.ENDPOINT + "/books_read_by_year"
+        if year is not None:
+            q += f"/{year}"
+        try:
+            r = requests.get(q)
+            res = r.json()
+        except requests.RequestException as e:
+            logging.error(e)
+        else:
+            _data = []
+            for year, pages, books in res["data"]:
+                q = self.ENDPOINT + f"/books?LastRead={year}"
+                try:
+                    tr = requests.get(q)
+                    tres = tr.json()
+                except requests.RequestException as e:
+                    logging.error(e)
                 else:
-                    print("<No Tags>")
+                    _data.extend(tres["data"])
+                    _template = ["" for i in range(len(tres["data"][0]))]
+                    _template[0] = f" **** {year} ****"
+                    _template[1] = f"Books = {books}"
+                    _template[2] = f"Pages = {pages}"
+                    _data.append(_template)
+            self._show_table(_data, tres["header"], self.MINIMAL_BOOK_INDEXES)
+
 
 if __name__ == "__main__":
     rpl = REPL_Tool()
