d2b6b58c63b37d931a1b93d30a081b4384e890ff
Scott Hendrickson <scott@drskippy.net>
2022-05-02 15:19:13 -0600

    missing comma in query

--- add_entry.php
+++ add_entry.php
@@ -7,7 +7,7 @@
 --->
 <?php
 
-echo ('<html><head><meta http-equiv="refresh" content="10; URL=./index.php">');
+echo ('<html><head><meta http-equiv="refresh" content="3; URL=./index.php">');
 echo ('<link href="./books.css" rel="stylesheet" type="text/css">');
 echo ('</head><body> <h3>Entry Added</h3>');
 include('./db_header.php');
@@ -23,10 +23,10 @@ $insert_str .= '(Title, Author, CopyrightDate, ISBNNumber, ISBNNumber13, Publish
 $insert_str .= 'CoverType, Pages, Location, Note, Recycled) ';
 $insert_str .= 'VALUES ("' . $_REQUEST['title'] . '","' . $_REQUEST['author'] . '","' . date("Y-m-d h:m:s", $dt);
 $insert_str .= '","' . $_REQUEST['isbnnumber'] . '","' . $_REQUEST['isbnnumber13'] . '","';
-$insert_str .= $_REQUEST['publishername'] . '","' . $_REQUEST['covertype'] . '","' . $_REQUEST['pages'];
+$insert_str .= $_REQUEST['publishername'] . '","' . $_REQUEST['covertype'] . '","' . $_REQUEST['pages'] . '","';
 // $insert_str .= '",0000-00-00,0000-00-00,"' . $_REQUEST['location'] . '","' . $_REQUEST['note'] . '","' . $recycled . '")';
 $insert_str .= $_REQUEST['location'] . '","' . $_REQUEST['note'] . '","' . $recycled . '")';
-echo($insert_str);
+//echo($insert_str);
 
 # echo '<hr>'.$insert_str;
 $result = @mysqli_query($dbcnx, $insert_str);
