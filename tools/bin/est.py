2d66893d52b7a8cf8863f1c06bc7e7a64ad6478a
DrSkippy <scott@aspenmesh.io>
2022-06-24 14:36:11 -0600

    refactoring the estimator

---
+++ tools/bin/est.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# Written by Scott Hendrickson
+#
+# Tools for estimating book reading completion date
+# Usage:
+# poetry run python bin/est.py ./book_estimates/command-control.txt
+###################################################
+import sys
+
+from bookdbtool.estimate_tools import *
+
+if len(sys.argv) < 2:
+    # setup and estimate
+    title = input("Enter book title: ")
+    fn = title.strip().lower().replace(" ", "_")
+    filename = f"./book_estimates/{fn}.txt"
+    start_date_str = input("Enter start date (YYYY-MM-DD): ")
+    start_date = datetime.datetime.strptime(start_date_str, FMT)
+    pages = int(input("Number of pages: "))
+    print(f"Book Title={title}")
+    print(f"File Name={filename}")
+    print(f"Start Date={start_date}")
+    with open(filename, "w") as of:
+        of.write(f"\"{title}\", {pages}\n")
+        for i in range(30):
+            dt = start_date + datetime.timedelta(days=i)
+            of.write("{},\n".format(dt.strftime(FMT)))
+else:
+    # estimate
+    filename = sys.argv[1]
+    data, header = reading_data(filename)
+    est_date, est_date_min, est_date_max = estimate_dates(data, header)
+    print("*"*80)
+    print(f"              Book: {header[0]}")
+    print("Estimated Complete: {}  Earliest: {}  Latest: {}".format(est_date.strftime(FMT), est_date_min.strftime(FMT),
+                                                                   est_date_max.strftime(FMT)))
+    print("*"*80)
--- tools/bookdbtool/estimate_tools.py
+++ tools/bookdbtool/estimate_tools.py
@@ -1,22 +1,11 @@
-#!/usr/bin/env python
-#
-# Written by Scott Hendrickson
-#
-# Tools for estimating book reading completion date
-# Usage:
-# poetry run python bin/estimator.py ./book_estimates/command-control.txt
-###################################################
 import csv
 import datetime
-import string
-import sys
 
 import numpy as np
 
 FMT = "%Y-%m-%d"
 
-
-def day_number(date):
+def day_number(data):
     # title, total pages to read
     # date, page
     # ...
@@ -49,26 +38,7 @@ def line_fit_and_estimate(data, total_pages):
     return int(est), est_range
 
 
-###################################################
-if len(sys.argv) < 2:
-    # setup and estimate
-    title = input("Enter book title: ")
-    fn = title.strip().lower().replace(" ", "_")
-    filename = f"./book_estimates/{fn}.txt"
-    start_date = input("Enter start date (YYYY-MM-DD): ")
-    start_date = datetime.datetime.strptime(start_date, FMT)
-    pages = int(input("Number of pages: "))
-    print(f"Book Title={title}")
-    print(f"File Name={filename}")
-    print(f"Start Date={start_date}")
-    with open(filename, "w") as of:
-        of.write(f"\"{title}\", {pages}\n")
-        for i in range(30):
-            dt = start_date + datetime.timedelta(days=i)
-            of.write("{},\n".format(dt.strftime(FMT)))
-else:
-    # estimate
-    filename = sys.argv[1]
+def reading_data(filename):
     header = None
     data = []
     with open(filename, "r") as book_file:
@@ -82,12 +52,13 @@ else:
                 else:
                     row.append(datetime.datetime.strptime(row[0], FMT))
                     data.append(row)
+    return data, header
+
+
+def estimate_dates(data, header):
     data, start_date = day_number(data)
     est, range = line_fit_and_estimate(data, float(header[1]))
     est_date = start_date + datetime.timedelta(days=est)
     est_date_max = start_date + datetime.timedelta(days=range[0])
     est_date_min = start_date + datetime.timedelta(days=range[1])
-
-    print(f"Book: {header[0]}")
-    print("Estimated complete: {}  Earliest: {} Latest: {}".format(est_date.strftime(FMT), est_date_min.strftime(FMT),
-                                                                   est_date_max.strftime(FMT)))
+    return est_date, est_date_min, est_date_max
