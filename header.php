ece355cbb2d2b84d25baff1329ca6290234f16c8
Scott Hendrickson <scott@drskippy.net>
2023-03-14 20:13:39 -0600

    more path errors

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 <link rel="shortcut icon" href="./favicon.ico" mce_href="./favicon.ico"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <link rel="stylesheet" href="./style/table-style.css">
-<link href="./style/colors.css" rel="stylesheet" type="text/css"/>
-<script src="./library/base.js"></script>
+<link href="/php_books_database/style/colors.css" rel="stylesheet" type="text/css"/>
+<script src="/php_books_database/library/base.js"></script>
 </head>
 <body>
 <div id="insertTopNavBar"></div>
