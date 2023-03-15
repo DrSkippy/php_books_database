fa682650c90e1e70d8acb211693b1d23788d567e
Scott Hendrickson <scott@drskippy.net>
2023-03-14 19:49:02 -0600

    bug in header path

--- header.php
+++ header.php
@@ -7,11 +7,11 @@
 --->
 <html>
 <head>
-<link href="colors.css" rel="stylesheet" type="text/css"/>
 <link rel="shortcut icon" href="./favicon.ico" mce_href="./favicon.ico"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="./js_reports/base.js"></script>
 <link rel="stylesheet" href="./style/table-style.css">
+<link href="./style/colors.css" rel="stylesheet" type="text/css"/>
+<script src="./js_reports/base.js"></script>
 </head>
 <body>
 <div id="insertTopNavBar"></div>
