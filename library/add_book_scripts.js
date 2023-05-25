b9726fe0ceb4d20f5635945343b06513f8261ca8
Scott Hendrickson <scott@aspenmesh.io>
2023-05-25 17:53:41 -0600

    structure of functions

--- library/add_book_scripts.js
+++ library/add_book_scripts.js
@@ -23,51 +23,49 @@ window.addEventListener('DOMContentLoaded', function() {
 // Handle form submission
 let form = document.forms.add;
 form.addEventListener('submit', function(event) {
-        event.preventDefault(); // prevent page reload
+    event.preventDefault(); // prevent page reload
+    console.log("submit called...");
+    // get form values
+    var author = document.forms["add"]["author"].value;
+    var title = document.forms["add"]["title"].value;
+    var isbnnumber = document.forms["add"]["isbnnumber"].value;
+    var isbnnumber13 = document.forms["add"]["isbnnumber13"].value;
+    var publishername = document.forms["add"]["publishername"].value;
+    var note = document.forms["add"]["note"].value;
+    var covertype = document.querySelector('input[name="covertype"]:checked').value;
+    var recycled = document.forms["add"]["recycled"].checked ? "yes" : "no";
+    var location = document.forms["add"]["location"].value;
+    var pages = document.forms["add"]["pages"].value;
+    var copyrightdate = document.forms["add"]["copyrightdate"].value;
 
-    function addEntry() {
-        // get form values
-        var author = document.forms["add"]["author"].value;
-        var title = document.forms["add"]["title"].value;
-        var isbnnumber = document.forms["add"]["isbnnumber"].value;
-        var isbnnumber13 = document.forms["add"]["isbnnumber13"].value;
-        var publishername = document.forms["add"]["publishername"].value;
-        var note = document.forms["add"]["note"].value;
-        var covertype = document.querySelector('input[name="covertype"]:checked').value;
-        var recycled = document.forms["add"]["recycled"].checked ? "yes" : "no";
-        var location = document.forms["add"]["location"].value;
-        var pages = document.forms["add"]["pages"].value;
-        var copyrightdate = document.forms["add"]["copyrightdate"].value;
-
-        // validate form values
-        if (author == "" || title == "" || isbnnumber == "" || publishername == "" || location == "" || pages == "" || copyrightdate == "") {
-          alert("Please fill in all required fields.");
-          return false;
-        }
-
-        if (isNaN(pages) || isNaN(copyrightdate)) {
-          alert("Please enter a valid number for Pages and Copyright.");
-          return false;
-        }
-
-        // submit form data
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-            alert("Entry added successfully!");
-            window.location.href = "./index.php";
-          }
-        };
-        message = "author=" + author + "&title=" + title + "&isbnnumber=" + isbnnumber + "&isbnnumber13=" +
-            isbnnumber13 + "&publishername=" + publishername + "&note=" + note + "&covertype=" + covertype +
-            "&recycled=" + recycled + "&location=" + location + "&pages=" + pages + "&copyrightdate=" + copyrightdate;
-
-        console.log(message)
-        // xhr.open("POST", "./add_entry.php", true);
-        xhr.open("POST", baseAPIUrl + "/add_books", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.send(message);
-
-        return false;
+    // validate form values
+    if (author == "" || title == "" || isbnnumber == "" || publishername == "" || location == "" || pages == "" || copyrightdate == "") {
+      alert("Please fill in all required fields.");
+      return false;
     }
+
+    if (isNaN(pages) || isNaN(copyrightdate)) {
+      alert("Please enter a valid number for Pages and Copyright.");
+      return false;
+    }
+
+    // submit form data
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        alert("Entry added successfully!");
+        window.location.href = "./index.php";
+      }
+    };
+    message = "author=" + author + "&title=" + title + "&isbnnumber=" + isbnnumber + "&isbnnumber13=" +
+        isbnnumber13 + "&publishername=" + publishername + "&note=" + note + "&covertype=" + covertype +
+        "&recycled=" + recycled + "&location=" + location + "&pages=" + pages + "&copyrightdate=" + copyrightdate;
+
+    console.log(message)
+    // xhr.open("POST", "./add_entry.php", true);
+    xhr.open("POST", baseAPIUrl + "/add_books", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(message);
+
+    return false;
 })
