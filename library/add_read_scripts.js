3da666260fd0ae4d41f73ddc2205fe1f2cdcd7f2
Scott Hendrickson <scott@drskippy.net>
2023-05-29 11:31:53 -0600

    added default date of today

--- library/add_read_scripts.js
+++ library/add_read_scripts.js
@@ -8,42 +8,8 @@ window.addEventListener('DOMContentLoaded', function() {
     topnavbar();
     createDetailTableRows();
     setval(book_id);
-    // Populate location dropdown with data from web API
-//    var url = baseApiUrl + "/books_search?BookCollectionId=" + book_id;
-//    console.log(url)
-//    $.getJSON(url, function (response) {
-//        var data = response['data'];
-//        console.log(data);
-//    }).catch(error => {
-//        console.error('Error fetching locations:', error);
-//    });
-//
-//    if (isbn_target != null) {
-//        // Populate fields from isbn.com
-//        var url = baseApiUrl + "/books_by_isbn";
-//        console.log(url);
-//        params = '{"isbn_list":["' + isbn_target + '"]}';
-//        console.log(params);
-//        var xhr = new XMLHttpRequest();
-//        xhr.open("POST", url, true);
-//        xhr.setRequestHeader("Content-type", "application/json");
-//        xhr.onreadystatechange = function () {
-//            if (this.readyState == 4 && this.status == 200) {
-//                console.log(xhr.responseText);
-//                var isbn = JSON.parse(xhr.responseText).book_records[0];
-//                // Set values
-//                document.forms["add"]["author"].value = isbn.Author;
-//                document.forms["add"]["title"].value = isbn.Title;
-//                document.forms["add"]["isbnnumber"].value = isbn.ISBNNumber;
-//                document.forms["add"]["isbnnumber13"].value = isbn.ISBNNumber13;
-//                document.forms["add"]["publishername"].value = isbn.PublisherName;
-//                document.forms["add"]["note"].value = isbn.Note;
-//                document.forms["add"]["pages"].value = isbn.Pages;
-//                document.forms["add"]["copyrightdate"].value = isbn.CopyrightDate;
-//            }
-//        };
-//        xhr.send(params);
-//    }
+    let now = new Date()
+    document.forms["add_read"]["read_date"].value = now.toISOString().split('T')[0];
 });
 
 form.addEventListener('submit', function(event) {
