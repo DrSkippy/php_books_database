0203b7339ed3228b30565affaf8c75d22ca760b0
Scott Hendrickson <scott@drskippy.net>
2023-05-30 20:04:57 -0600

    continue_flag name change due to conflict

--- library/book_scripts.js
+++ library/book_scripts.js
@@ -1,30 +1,32 @@
 $(document).ready(function () {
     topnavbar();
+    
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    console.log(queryString)
+    
     var url = baseApiUrl + "/books_search?";
-    var continue_flag = false;
+    var params_continue_flag = false;
+    
     if (urlParams.has("author") && urlParams.get("author").trim() != "") {
         url += "Author=" + urlParams.get("author").trim();
-        continue_flag = true;
+        params_continue_flag = true;
     }
-    console.log(url);
+
     if (urlParams.has("title") && urlParams.get("title").trim() != "") {
-        if (continue_flag) {
+        if (params_continue_flag) {
             url += "&";
         }
         url += "Title=" + urlParams.get("title").trim();
-        continue_flag = true;
+        params_continue_flag = true;
     }
-    console.log(url);
+
     if (urlParams.has("tags") && urlParams.get("tags").trim() != "") {
-        if (continue_flag) {
+        if (params_continue_flag) {
             url += "&";
         }
         url += "Tags=" + urlParams.get("tags").trim();
     }
-    console.log(url);
+
     $.getJSON(url, function (data) {
         var obj = data['data'];
         for (var i = 0; i < obj.length; i++) {
