0420911a34c93ffed7d31524e9819418e9478cf9
Scott Hendrickson <scott@aspenmesh.io>
2023-06-01 10:19:18 -0600

    sorting numbers not strings

--- library/base.js
+++ library/base.js
@@ -107,13 +107,13 @@ function sortTable(n) {
             /* Check if the two rows should switch place,
             based on the direction, asc or desc: */
             if (dir == "asc") {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                if (x.innerHTML.parseInt() > y.innerHTML.parseInt()) {
                     // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
                     break;
                 }
             } else if (dir == "desc") {
-                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                if (x.innerHTML.parseInt() < y.innerHTML.parseInt()) {
                     // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
                     break;
