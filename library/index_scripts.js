e0b661fa6cc1226c5d1d702943336c642d466556
Scott Hendrickson <scott@drskippy.net>
2024-02-04 19:54:07 -0700

    got generated html rows correct

--- library/index_scripts.js
+++ library/index_scripts.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
         for (var i = 0; i < obj.length; i++) {
             var url = new URL("/php_books_database/js_reports/books_detail.html?author=&title=" +
                 obj[i][2].toString().substring(0,20) + "&tags=", window.location);
-            var tr = "<tr><td><a href=\"" + url.href + "\">" + obj[i][2] + "\"</a></td>" +
+            var tr = "<tr><td><a href=\"" + url.href + "\">" + obj[i][2] + "</a></td>" +
                 "<td>" + obj[i][1] + "</td></tr>";
             console.log(tr);
             $("#mytable-recent").append(tr);
