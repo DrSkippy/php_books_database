ae7dd8521fbc22af79897e70d0401b25e4d4ba8c
Scott Hendrickson <scott@drskippy.net>
2024-02-04 19:52:12 -0700

    debugging

--- library/index_scripts.js
+++ library/index_scripts.js
@@ -19,11 +19,10 @@ $(document).ready(function () {
         for (var i = 0; i < obj.length; i++) {
             var url = new URL("/php_books_database/js_reports/books_detail.html?author=&title=" +
                 obj[i][2].toString().substring(0,20) + "&tags=", window.location);
-            console.log(url);
-            var tr = "<tr><td><a href=" + url.href + ">" + obj[i][2] + "</a></td>" +
+            var tr = "<tr><td><a href=\"" + url.href + "\">" + obj[i][2] + "\"</a></td>" +
                 "<td>" + obj[i][1] + "</td></tr>";
             console.log(tr);
-            $("#recent-table").append(tr);
+            $("#mytable-recent").append(tr);
         }
     });
 })
